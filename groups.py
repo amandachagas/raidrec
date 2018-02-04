fe68dd3d57fd36db3f8017fcdaf9e84d2cde3a51
Dourado <danilodourado.maqhin@gmail.com>
2018-02-04 20:02:23 -0300

    [cod] changed aws function

--- groups.py
+++ groups.py
@@ -25,11 +25,15 @@ def multiplicative(matrix):
 def average_without_misery(matrix, threshold):
     arr = []
     for row in matrix:
-        wm = [n for n in row if n >= threshold]
-        if(wm):
-            arr.append( sum(wm)/float(len(wm)) )
+        if min(row)<threshold:
+            arr.append(min(row))
         else:
-            arr.append(1.0)
+            arr.append( sum(row)/float(len(row)) )
+        # wm = [n for n in row if n >= threshold]
+        # if(wm):
+        #     arr.append( sum(wm)/float(len(wm)) )
+        # else:
+        #     arr.append(1.0)
     return arr
 
 def run_strategies(matrix, awm_threshold):
